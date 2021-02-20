614adb2dc7b585d6a793184aabcdc5e3b7167d3d
Mason <38963371+nosamproductions@users.noreply.github.com>
2021-02-20 12:35:53 -0600

    Some refactoring, added pop_random

--- src/cpp/thread_safe_vector.hpp
+++ src/cpp/thread_safe_vector.hpp
@@ -34,13 +34,19 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 #include<vector>
 #include<mutex>
+#include <random>
 #include<condition_variable>
+
+// A thread safe vector designed in the context of a use case involving lots of threads pushing and a small number of threads popping
 
 // Anything that involves iterating needs to be its own method in this class since you
 // don't really don't want multiple threads iterating and modifying the vector at the same time
 template <typename T>
 class thread_safe_vector {
 public:
+	thread_safe_vector () :
+		generator_(rd_())
+	{}
 	std::atomic_bool empty() {
 		std::lock_guard<std::mutex> lk(m_);
 		return vec_.empty();
@@ -52,14 +58,21 @@ public:
 	T pop(std::size_t idx) {
 		std::unique_lock<std::mutex> lk(m_);
 		cv_.wait(lk, [this]() {return !vec_.empty(); });
-		T out = vec_.at(idx);
-		vec_.erase(vec_.begin()+idx);
-		return out;
+		return pop_non_thread_safe(idx);
 	}
+	T pop_random() {
+		std::unique_lock<std::mutex> lk(m_);
+		cv_.wait(lk, [this]() {return !vec_.empty(); });
+		std::uniform_int_distribution<std::size_t> distribution(0, vec_.size()-1);
+		return pop_non_thread_safe(distribution(generator_));
+	}
+	template<bool do_notify = true>
 	void push_back(T val) {
 		std::lock_guard<std::mutex> lk(m_);
 		vec_.push_back(val);
-		notify();
+		if (do_notify) {
+			notify();
+		}
 	}
 	void erase_index(std::size_t idx) {
 		std::lock_guard<std::mutex> lk(m_);
@@ -108,4 +121,12 @@ private:
 	std::vector<T> vec_;
 	std::mutex m_;
 	std::condition_variable cv_;
+	std::random_device rd_;
+	std::mt19937 generator_;
+	T pop_non_thread_safe(std::size_t idx) {
+		T out = vec_.at(idx);
+		vec_.erase(vec_.begin() + idx);
+		return out;
+	}
+
 };
